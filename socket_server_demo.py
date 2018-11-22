85fb1af5161ca0627e233302826ac2a9486c8880
Lingfliu <lingfeng.liu@163.com>
2018-11-22 18:25:10 +0800

    seperated tx/rx thread

--- socket_server_demo.py
+++ socket_server_demo.py
@@ -12,33 +12,33 @@ except socket.error as msg:
 
 sock.listen(0)
 
-cli_threads = {}
+tx_threads = {}
+rx_threads = {}
 
-def clientThread(conn):
+def writeThread(conn):
     cnt = 0
-    reply = [0x00, 0xaa, 0x00, 0xcc, 30, 0x00, 0x00, 0x00]
-    # conn.sendall(bytearray(reply))
-    conn.sendall('hello'.encode(encoding='ASCII'))
     while True:
         time.sleep(0.0001)
-        # conn.sendall(bytearray(reply))
-        # conn.sendall('hello'.encode(encoding='ASCII'))
+        conn.sendall(('received ' + str(cnt)).encode(encoding='ASCII'))
+        cnt += 1
+        print("sending idx=", cnt)
+
+def readThread(conn):
+    while True:
+        time.sleep(0.0001)
         data = conn.recv(1024)
         print("received data from", data)
-        reply = data
         if not data:
             break
-        # conn.sendall(('received '+str(cnt)).encode(encoding='ASCII'))
-        # cnt += 1
-        # print("sending idx=", cnt)
-            # break
-        # conn.sendall(bytearray(reply))
 
 while True:
     conn, addr = sock.accept()
     print("client connected, addr ", addr)
-    thrd = threading.Thread(target=clientThread, args=(conn,))
-    cli_threads[addr] = thrd
-    thrd.start()
+    writethrd = threading.Thread(target=writeThread, args=(conn,))
+    readthrd = threading.Thread(target=readThread, args=(conn,))
+    tx_threads[addr] = writethrd
+    rx_threads[addr] = readthrd
+    writethrd.start()
+    readthrd.start()
 
 
